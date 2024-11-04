cf7976f2a916ea19c93e6d5796063ccedaf3d013
Dustin Lodge <lodge.dustin@outlook.com>
2024-11-04 00:29:30 -0700

    updated product.js to store items to array

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,22 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+// :::WEEK 1::: add 'product' to array for cart output
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cartItems = getLocalStorage("so-cart") || [];
+
+  // if NOT array, make it so!
+  if (!Array.isArray(cartItems)) {
+    cartItems = [];
+  }
+
+  // push cart item to array
+  cartItems.push(product);
+
+  // store array in local storage for output
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
