8c1fc1b0598aeed5d2fa9e61dbb29303143d4bdb
Dustin Lodge <lodge.dustin@outlook.com>
2024-11-19 22:12:13 -0700

    completed w02 group details page

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,14 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 // :::WEEK 1::: add 'product' to array for cart output
 function addProductToCart(product) {
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,3 +1,5 @@
+
+
 // wrapper for querySelector...returns matching element
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
@@ -13,6 +15,16 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
+// get param string
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  console.log(product)
+  return product;
+}
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
@@ -21,3 +33,4 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
