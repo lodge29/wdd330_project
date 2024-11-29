1a74c136ed490bde997f088574a89d334de6926f
Dustin Lodge <lodge.dustin@outlook.com>
2024-11-29 13:58:32 -0700

    maintenance

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,11 +1,10 @@
-import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
 
 const cart = new ShoppingCart("so-cart", ".product-list");
 cart.renderCartContents();
-
 
 /*
 import { getLocalStorage } from "./utils.mjs";
